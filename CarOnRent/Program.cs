dccb645e7b8ae96077f744054eb856d708bf365e
Sharmila Paul <M1092926@mindtree.com>
2022-11-21 12:31:07 +0530

    Initial commit 1

---
+++ CarOnRent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CarOnRent
+{
+    class Car
+    {
+        String name, model;
+        int purchase_year;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
