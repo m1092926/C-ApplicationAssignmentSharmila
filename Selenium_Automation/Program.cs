678ee748dd192f12483747ac0628044fe4bb3139
Sharmila Paul <M1092926@mindtree.com>
2022-11-25 19:17:15 +0530

    Commit selenium 1

---
+++ Selenium_Automation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Selenium_Automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
