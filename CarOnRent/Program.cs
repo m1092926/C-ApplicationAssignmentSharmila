bc649bfb13468af7f5231a11f8e1c2f853d33b5a
Sharmila Paul <M1092926@mindtree.com>
2022-11-25 12:07:58 +0530

    Initial commit C# assignment 1

--- CarOnRent/Program.cs
+++ CarOnRent/Program.cs
@@ -2,14 +2,73 @@
 
 namespace CarOnRent
 {
-    class Car
+    interface IUnits
+    {
+        int display();
+    }
+  public partial class Car: IUnits
     {
         String name, model;
         int purchase_year;
+        public virtual decimal ProjectedRentalAmt()
+        {
+            decimal y = 1.0M;
+            Console.WriteLine("This is subtraction method");
+            return y;
+         }
+        public void addition()
+        {
+            Console.WriteLine("This is addition method");
+        }
+        public override string ToString()
+        {
+            return   name + " " + model +" "+ purchase_year;
+        }
+
+        public int display()
+        {
+            int number_of_units = 0;
+            Console.WriteLine(number_of_units);
+            return number_of_units;
+        }
+     public   class MultiUnit: Car, IUnits
+
+        {
+            String complex_name;
+            int number_of_units, rent_amt_per_unit;
+            public int GetNumUnits()
+            {
+                int x = 0;
+                Console.WriteLine(x);
+                return x;
+            }
+            public override string ToString()
+            {
+                return complex_name + " " + number_of_units + " " + rent_amt_per_unit;
+            }
+            public override decimal ProjectedRentalAmt()
+            {
+                decimal y = 1.0M;
+                Console.WriteLine("This is subtraction method");
+                y = rent_amt_per_unit * number_of_units * 12;
+                return y;
+            }
+        }
+
+
+      
+       
+
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Car car = new Car();
+           car. ProjectedRentalAmt();
+            car.addition();
+            car.ToString();
+            car.display();
+
         }
     }
 }
