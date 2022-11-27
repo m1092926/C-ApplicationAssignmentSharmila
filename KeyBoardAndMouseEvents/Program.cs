b4d161d42494c1225a67f1c157b2b21bf611098f
Sharmila Paul <M1092926@mindtree.com>
2022-11-27 21:54:26 +0530

    drag  & drop commit 1

---
+++ KeyBoardAndMouseEvents/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using FindsByAttribute = SeleniumExtras.PageObjects.FindsByAttribute;
+using How = SeleniumExtras.PageObjects.How;
+using SeleniumExtras.WaitHelpers;
+namespace KeyBoardAndMouseEvents
+{
+    [TestFixture]
+  public   class Program
+    {
+       [Test]
+        public void example1() {
+            var driver = new FirefoxDriver();
+            var actions = new Actions(driver);
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(6));
+
+            driver.Navigate().GoToUrl("https://jqueryui.com/droppable");
+          
+           wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+            IWebElement targetElement = driver.FindElement(By.Id("draggable"));
+            IWebElement sourceElement = driver.FindElement(By.Id("droppable"));
+            actions.DragAndDrop(sourceElement,targetElement).Perform();
+
+            Assert.AreEqual("Dropped!",targetElement.Text);
+
+        }
+
+
+
+    }
+
+}
+
+
+    
--- Selenium_Automation/Program.cs
+++ Selenium_Automation/Program.cs
@@ -10,14 +10,14 @@ namespace Selenium_Automation
         {
             IWebDriver driver = new ChromeDriver("C:\\C#assignment\\Chrome\\chromedriver.exe");
 
-            // This will open up the URL
             driver.Url = "https://demo.automationtesting.in/Register.html/";
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.XPath( "//input[@ng-model='FirstName']")).SendKeys("Sharmila");
+            driver.FindElement(By.XPath("//input[@ng-model='FirstName']")).SendKeys("Sharmila");
             driver.FindElement(By.XPath("//input[@ng-model='LastName']")).SendKeys("Paul");
             driver.FindElement(By.XPath("//textarea[@ng-model='Adress']")).SendKeys("Click Nicon studio");
             WebElement upload_file = (WebElement)driver.FindElement(By.XPath("//input[@type='file']"));
-            upload_file.SendKeys("C:/Users/Sonali/Desktop/upload.png");
+            upload_file.SendKeys("C:\\C#assignment\\Chrome\\download.jfif");
+
         }
     }
 }
